1a254fff36405a62e1625c9405a3fde2925614cc
Codethestars <agus.widjaja.usa@gmail.com>
2024-11-25 11:49:39 -0800

    Work on local with correct template path

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
-# Tell Flask where to find the templates folder
-app = Flask(__name__, template_folder="../templates")
+# Point to templates in root folder
+app = Flask(__name__, template_folder="templates")
 
 @app.route("/")
 def home():
