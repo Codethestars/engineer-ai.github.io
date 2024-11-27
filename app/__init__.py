e68dcc8374b4e22dd10242a80f7da877e4db055a
Codethestars <agus.widjaja.usa@gmail.com>
2024-11-26 21:57:08 -0800

    Fix local template path issue

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
-import os
 from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, 
+           template_folder='../templates',  # Point to templates directory
+           static_folder='../static')       # Point to static directory
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(port=os.environ.get('PORT', 5000))
+    app.run()
